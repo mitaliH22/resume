d43520a993c35ffc6b0e8229a91b013e9c8c8219
Mitali Harsh <mitali.harsh@sunarctechnologies.com>
2023-06-28 18:12:04 +0530

    project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { SocialsComponent } from './component/socials/socials.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './component/resume/resume.component';
+import { CardComponent } from './component/card/card.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { PageComponent } from './component/page/page.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SocialsComponent,
+    FooterComponent,
+    ResumeComponent,
+    CardComponent,
+    ProjectsComponent,
+    PageComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
